772f62d845497bf69b6205bfedef291f08c0f41c
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2023-08-26 15:58:50 +0200

    fix example

--- examples/hello_world.rs
+++ examples/hello_world.rs
@@ -1,24 +1,43 @@
 #![no_std]
 
+extern crate alloc;
+
+use alloc::ffi::CString;
+use alloc::format;
+use core::alloc::{GlobalAlloc, Layout};
 use core::ptr;
 
 use libm::{cosf, sinf};
 
-use crankit_sys::{LCDBitmapFlip, LCDColor, LCDSolidColor, LCDSprite, PDSystemEvent, PlaydateAPI};
+use crankit_sys::{
+    playdate_graphics, playdate_sprite, playdate_sys, LCDBitmapFlip, LCDColor, LCDSolidColor,
+    LCDSprite, PDSystemEvent, PlaydateAPI,
+};
 
+static mut PLAYDATE: Option<*mut PlaydateAPI> = None;
 static mut GAME: Option<Game> = None;
 
+unsafe fn gfx() -> *const playdate_graphics {
+    (*PLAYDATE.unwrap()).graphics
+}
+
+unsafe fn system() -> *const playdate_sys {
+    (*PLAYDATE.unwrap()).system
+}
+
+unsafe fn sprites() -> *const playdate_sprite {
+    (*PLAYDATE.unwrap()).sprite
+}
+
 struct Game {
-    playdate: *mut PlaydateAPI,
     sprite: *mut LCDSprite,
 }
 
 impl Game {
-    unsafe fn init(playdate: *mut PlaydateAPI) -> Self {
-        let gfx = (*playdate).graphics;
-        let bitmap = (*gfx).newBitmap.unwrap()(10, 10, LCDSolidColor::kColorClear as LCDColor);
-        (*gfx).pushContext.unwrap()(bitmap);
-        (*gfx).fillEllipse.unwrap()(
+    unsafe fn init() -> Self {
+        let bitmap = (*gfx()).newBitmap.unwrap()(10, 10, LCDSolidColor::kColorWhite as LCDColor);
+        (*gfx()).pushContext.unwrap()(bitmap);
+        (*gfx()).fillEllipse.unwrap()(
             0,
             0,
             10,
@@ -27,28 +46,26 @@ impl Game {
             360.0,
             LCDSolidColor::kColorBlack as LCDColor,
         );
-        (*gfx).popContext.unwrap()();
-        let sprite_manager = (*playdate).sprite;
-        let sprite = (*sprite_manager).newSprite.unwrap()();
-        (*sprite_manager).setImage.unwrap()(sprite, bitmap, LCDBitmapFlip::kBitmapUnflipped);
-        (*sprite_manager).moveTo.unwrap()(sprite, 200.0, 120.0);
-        Self { playdate, sprite }
+        (*gfx()).popContext.unwrap()();
+        let sprite = (*sprites()).newSprite.unwrap()();
+        (*sprites()).setImage.unwrap()(sprite, bitmap, LCDBitmapFlip::kBitmapUnflipped);
+        (*sprites()).moveTo.unwrap()(sprite, 200.0, 120.0);
+        (*sprites()).addSprite.unwrap()(sprite);
+        Self { sprite }
     }
 
     unsafe fn update(&mut self) {
-        let crank_angle = (*(*self.playdate).system).getCrankAngle.unwrap()().to_radians();
-        let dx = cosf(crank_angle);
-        let dy = sinf(crank_angle);
+        (*gfx()).clear.unwrap()(LCDSolidColor::kColorWhite as LCDColor);
+        let crank_angle = (*system()).getCrankAngle.unwrap()().to_radians();
         let mut x = 0.0;
         let mut y = 0.0;
-        let sprite_manager = (*self.playdate).sprite;
-        (*sprite_manager).getPosition.unwrap()(
-            self.sprite,
-            ptr::addr_of_mut!(x),
-            ptr::addr_of_mut!(y),
-        );
-        (*sprite_manager).moveBy.unwrap()(self.sprite, dx, dy);
-        (*sprite_manager).drawSprites.unwrap()();
+        (*sprites()).getPosition.unwrap()(self.sprite, ptr::addr_of_mut!(x), ptr::addr_of_mut!(y));
+        x = (x + sinf(crank_angle)).clamp(5.0, 395.0);
+        y = (y - cosf(crank_angle)).clamp(5.0, 235.0);
+        (*sprites()).moveTo.unwrap()(self.sprite, x, y);
+        let string = CString::new(format!("position: ({x:.0}, {y:.0})")).unwrap();
+        (*system()).logToConsole.unwrap()(string.as_ptr());
+        (*sprites()).drawSprites.unwrap()();
     }
 }
 
@@ -60,8 +77,9 @@ extern "C" fn eventHandler(
 ) -> core::ffi::c_int {
     if event == PDSystemEvent::kEventInit {
         unsafe {
-            GAME = Some(Game::init(playdate));
-            (*(*playdate).system).setUpdateCallback.unwrap()(Some(update), ptr::null_mut())
+            PLAYDATE = Some(playdate);
+            GAME = Some(Game::init());
+            (*system()).setUpdateCallback.unwrap()(Some(update), ptr::null_mut())
         }
     }
     0
@@ -73,6 +91,21 @@ extern "C" fn update(_user_data: *mut core::ffi::c_void) -> i32 {
     }
     1
 }
+
+pub struct Allocator;
+
+unsafe impl GlobalAlloc for Allocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        (*system()).realloc.unwrap()(ptr::null_mut(), layout.size()).cast::<u8>()
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, _: Layout) {
+        (*system()).realloc.unwrap()(ptr.cast::<core::ffi::c_void>(), 0);
+    }
+}
+
+#[global_allocator]
+static mut GLOBAL_ALLOCATOR: Allocator = Allocator;
 
 #[panic_handler]
 #[cfg(not(test))]
